cab85bc438f2193469c9fe1d6cccd685bef507e4
jm-avila <jm.avila1309@icloud.com>
2019-11-30 10:14:43 +0100

    get Various Titles and Ratings in place

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const { getTitleAndRating } = require("./scrappersByWebsite/imdb");
+const {
+  getTitleAndRating,
+  getVariousTitlesAndRatings
+} = require("./scrappersByWebsite/imdb");
 
-getTitleAndRating("https://www.imdb.com/title/tt4158110/?ref_=nv_sr_srsg_0");
+const URL = "https://www.imdb.com/title/tt4158110/";
+const URLS = [
+  URL,
+  "https://www.imdb.com/title/tt4047350/?ref_=nv_sr_srsg_0",
+  "https://www.imdb.com/title/tt3042408/?ref_=fn_al_tt_4"
+];
+getTitleAndRating(URL).then(console.log);
+// getVariousTitlesAndRatings(URLS).then(console.log);
--- scrappersByWebsite/imdb.js
+++ scrappersByWebsite/imdb.js
@@ -38,10 +38,33 @@ const getTitleAndRating = async URL => {
   let rating = $('span[itemprop="ratingValue"]').text();
   let image = $('div[class="poster"] > a > img').attr("src");
   let ratingCount = $('span[itemprop="ratingCount"]').text();
+  let releaseDate = $('a[title="See more release dates"]')
+    .text()
+    .trim();
+  let genres = $('div[class="title_wrapper"] a[href^="/search/title?genres"]')
+    .map((i, element) => $(element).text())
+    .toArray();
+  return {
+    title,
+    rating,
+    image,
+    ratingCount,
+    releaseDate,
+    genres
+  };
+};
 
-  console.log(title, rating, ratingCount, image);
+const getVariousTitlesAndRatings = async URLS => {
+  const requestsArray = URLS.map(url => getTitleAndRating(url));
+  const responsesArray = [];
+  for (let request of requestsArray) {
+    const response = await request;
+    responsesArray.push(response);
+  }
+  return responsesArray;
 };
 
 module.exports = {
-  getTitleAndRating: getTitleAndRating
+  getTitleAndRating: getTitleAndRating,
+  getVariousTitlesAndRatings: getVariousTitlesAndRatings
 };
